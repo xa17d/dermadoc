57f5e475ee1571860e08fca70cf4b81453a03647
Daniel Gehrer <daniel.git@xa1.at>
2015-11-11 23:32:30 +0100

    create empty server project with entities dependency

---
+++ server/src/main/java/at/tuwien/telemedizin/dermadoc/server/Application.java
@@ -0,0 +1,10 @@
+package at.tuwien.telemedizin.dermadoc.server;
+
+/**
+ * Created by daniel on 11.11.2015.
+ */
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
