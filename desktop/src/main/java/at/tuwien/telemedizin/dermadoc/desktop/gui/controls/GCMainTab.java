b29379976fa90cc0c8d37cd0762691199a09677a
Lucas Dobler <lucas@ldob.eu>
2015-11-16 14:32:35 +0100

    chat bubbles - first try;

--- desktop/src/main/java/at/tuwien/telemedizin/dermadoc/desktop/gui/controls/GCMainTab.java
+++ desktop/src/main/java/at/tuwien/telemedizin/dermadoc/desktop/gui/controls/GCMainTab.java
@@ -4,13 +4,14 @@ import at.tuwien.telemedizin.dermadoc.desktop.gui.Controller;
 import at.tuwien.telemedizin.dermadoc.entities.Case;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Label;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.TitledPane;
+import javafx.geometry.HPos;
+import javafx.scene.control.*;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
+import java.net.URL;
 
 /**
  * Created by Lucas on 14.11.2015.
@@ -43,10 +44,32 @@ public class GCMainTab extends Tab {
 
         this.setText(aCase.getPatient().getName());
         tpPatientOverview.setContent(new GCMainTabOverview(aCase.getPatient()));
+
+        //Chat window
+        GridPane chat = new GridPane();
+        chat.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+
+        ColumnConstraints c1 = new ColumnConstraints();
+        c1.setPercentWidth(100);
+        chat.getColumnConstraints().add(c1);
+
+        for (int i = 0; i < 20; i++) {
+            Label chatMessage = new Label("Hi " + i);
+            String style = (i % 2 == 0 ? "chat-bubblepatient" : "chat-bubblephysician");
+            chatMessage.getStyleClass().add(style);
+            GridPane.setHalignment(chatMessage, i % 2 == 0 ? HPos.LEFT : HPos.RIGHT);
+            chat.addRow(i, chatMessage);
+        }
+
+        ScrollPane scroll = new ScrollPane(chat);
+        scroll.setFitToWidth(true);
+        pnInput.getChildren().add(scroll);
+
+        scroll.getStylesheets().add(getClass().getResource("chat.css").toExternalForm());
     }
 
     @FXML
     private void newFreetext() {
-        pnInput.getChildren().add(new Label("new freetext"));
+        //pnInput.getChildren().add(new Label("new freetext"));
     }
 }
