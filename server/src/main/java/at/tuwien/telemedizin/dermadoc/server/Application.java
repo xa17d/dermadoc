1cb9b0faeab21b1db5c728b485c4994ad4daf7e9
Daniel Gehrer <daniel.git@xa1.at>
2015-12-16 14:28:28 +0100

    set datasource platform to postgres

--- server/src/main/java/at/tuwien/telemedizin/dermadoc/server/Application.java
+++ server/src/main/java/at/tuwien/telemedizin/dermadoc/server/Application.java
@@ -16,8 +16,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @EnableWebMvc
 @ComponentScan
-//@EnableAutoConfiguration
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration
 @EnableConfigurationProperties
 public class Application {
     public static void main(String[] args) {
