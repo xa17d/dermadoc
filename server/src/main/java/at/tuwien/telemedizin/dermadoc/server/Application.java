84c44e2dadd9950a808e474f25ea80aed67697c2
Daniel Gehrer <daniel.git@xa1.at>
2015-12-16 14:21:18 +0100

    disable auto DataSource configuration (does not work with Postgres)

--- server/src/main/java/at/tuwien/telemedizin/dermadoc/server/Application.java
+++ server/src/main/java/at/tuwien/telemedizin/dermadoc/server/Application.java
@@ -3,6 +3,7 @@ package at.tuwien.telemedizin.dermadoc.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +16,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @EnableWebMvc
 @ComponentScan
-@EnableAutoConfiguration
+//@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @EnableConfigurationProperties
 public class Application {
     public static void main(String[] args) {
