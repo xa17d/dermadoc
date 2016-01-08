1d59c630a268266c8d064aa8e92724564c7aed89
Fabian Filip <fabian.filip@inode.at>
2016-01-08 19:29:11 +0100

    android - Help-Activity forwards to Mail-Client

--- app/app/src/main/java/at/tuwien/telemedizin/dermadoc/app/activities_fragments/help/HelpActivity.java
+++ app/app/src/main/java/at/tuwien/telemedizin/dermadoc/app/activities_fragments/help/HelpActivity.java
@@ -1,5 +1,7 @@
 package at.tuwien.telemedizin.dermadoc.app.activities_fragments.help;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,11 +24,21 @@ public class HelpActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
+                // open List with Mail clients
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setType("text/plain");
+                intent.setData(Uri.parse("mailto:"));
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"support@dermadoc.example.com"});
+                intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.mail_subject));
+//                intent.putExtra(Intent.EXTRA_TEXT, "I'm email body.");
+
+                startActivity(Intent.createChooser(intent, getString(R.string.mail_intent_chooser_title)));
             }
         });
-        fab.setVisibility(View.GONE);
+
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
