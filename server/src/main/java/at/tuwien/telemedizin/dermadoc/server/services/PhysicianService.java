bfc6f4637abe102011b5098c2218c3be977fc0b7
Daniel Gehrer <daniel.git@xa1.at>
2016-01-10 23:11:46 +0100

    physicianService implemented

---
+++ server/src/main/java/at/tuwien/telemedizin/dermadoc/server/services/PhysicianService.java
@@ -0,0 +1,87 @@
+package at.tuwien.telemedizin.dermadoc.server.services;
+
+import at.tuwien.telemedizin.dermadoc.entities.GeoLocation;
+import at.tuwien.telemedizin.dermadoc.entities.Physician;
+import at.tuwien.telemedizin.dermadoc.entities.rest.UserList;
+import at.tuwien.telemedizin.dermadoc.server.persistence.dao.hibernate.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+/**
+ * Created by daniel on 10.01.2016.
+ */
+@Service
+public class PhysicianService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private int nearestCount = 10;
+
+    /**
+     * List n nearest physician for a specific GeoLocation (n defined by constant in this class - default 10)
+     * @param geoLocation Location to search at
+     * @return Ordered list with nearest Physicians. Nearest first.
+     */
+    public UserList listNearestPhysicians(GeoLocation geoLocation) {
+        Iterable<Physician> physicians = userRepository.listPhysicians();
+
+        // Calculate Distances to all Physicians
+
+        ArrayList<PhysicianDistance> physicianDistances = new ArrayList<>();
+
+        for (Physician p : physicians) {
+            if (p.getLocation() != null) {
+                PhysicianDistance d;
+
+                if (geoLocation == null || p.getLocation() == null) {
+                    d = new PhysicianDistance(p, Double.MAX_VALUE);
+                }
+                else {
+                    d = new PhysicianDistance(p, GeoLocation.Distance(geoLocation, p.getLocation()));
+                }
+
+                physicianDistances.add(d);
+            }
+        }
+
+        // Sort by distance
+
+        Collections.sort(physicianDistances, new Comparator<PhysicianDistance>() {
+            @Override
+            public int compare(PhysicianDistance o1, PhysicianDistance o2) {
+                if (o1.getDistance() < o2.getDistance()) return -1;
+                if (o1.getDistance() > o2.getDistance()) return 1;
+                return 0;
+            }
+        });
+
+        // Generate result list with nearest physicians
+
+        UserList result = new UserList();
+
+        int resultSize = Math.min(nearestCount, physicianDistances.size());
+        for (int i=0; i<resultSize; i++) {
+            result.add(physicianDistances.get(i).getPhysician());
+        }
+
+        return result;
+     }
+
+    private class PhysicianDistance {
+        public PhysicianDistance(Physician physician, double distance) {
+            this.physician = physician;
+            this.distance = distance;
+        }
+
+        private Physician physician;
+        private double distance;
+
+        public Physician getPhysician() { return physician; }
+        public double getDistance() { return distance; }
+    }
+}
---
+++ server/src/test/java/at/tuwien/telemedizin/dermadoc/server/services/PhysicianServiceTest.java
@@ -0,0 +1,90 @@
+package at.tuwien.telemedizin.dermadoc.server.services;
+
+import at.tuwien.telemedizin.dermadoc.entities.Case;
+import at.tuwien.telemedizin.dermadoc.entities.GeoLocation;
+import at.tuwien.telemedizin.dermadoc.entities.Physician;
+import at.tuwien.telemedizin.dermadoc.entities.casedata.CaseData;
+import at.tuwien.telemedizin.dermadoc.entities.casedata.CaseInfo;
+import at.tuwien.telemedizin.dermadoc.entities.casedata.PhotoMessage;
+import at.tuwien.telemedizin.dermadoc.entities.casedata.TextMessage;
+import at.tuwien.telemedizin.dermadoc.entities.rest.UserList;
+import at.tuwien.telemedizin.dermadoc.server.Application;
+import at.tuwien.telemedizin.dermadoc.server.persistence.dao.hibernate.CaseDataRepository;
+import at.tuwien.telemedizin.dermadoc.server.persistence.dao.hibernate.CaseRepository;
+import at.tuwien.telemedizin.dermadoc.server.persistence.dao.hibernate.MedicationRepository;
+import at.tuwien.telemedizin.dermadoc.server.persistence.dao.hibernate.UserRepository;
+import junit.framework.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Calendar;
+
+/**
+ * Created by daniel on 10.01.2016.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
+@Transactional
+public class PhysicianServiceTest {
+    @Autowired
+    private PhysicianService physicianService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    public void listNearestPhysiciansTest() throws Exception {
+
+        Physician pBregenz = new Physician();
+        pBregenz.setName("Bregenz");
+        pBregenz.setMail("Bregenz");
+        pBregenz.setPassword("Bregenz");
+        pBregenz.setLocation(new GeoLocation("Bregenz", 47.5071327, 9.7169709));
+        userRepository.save(pBregenz);
+
+        Physician pInnsbruck = new Physician();
+        pInnsbruck.setName("Innsbruck");
+        pInnsbruck.setMail("Innsbruck");
+        pInnsbruck.setPassword("Innsbruck");
+        pInnsbruck.setLocation(new GeoLocation("Innsbruck", 47.2855502, 11.3087505));
+        userRepository.save(pInnsbruck);
+
+        Physician pSalzburg = new Physician();
+        pSalzburg.setName("Salzburg");
+        pSalzburg.setMail("Salzburg");
+        pSalzburg.setPassword("Salzburg");
+        pSalzburg.setLocation(new GeoLocation("Salzburg", 47.8028623, 13.0214107));
+        userRepository.save(pSalzburg);
+
+        Physician pWien = new Physician();
+        pWien.setName("Wien");
+        pWien.setMail("Wien");
+        pWien.setPassword("Wien");
+        pWien.setLocation(new GeoLocation("Wien", 48.220778, 16.3100205));
+        userRepository.save(pWien);
+
+        UserList list;
+
+        list = physicianService.listNearestPhysicians(new GeoLocation("Innsbruck", 47.2855502, 11.3087505));
+        Assert.assertEquals("Item 0", "Innsbruck", list.get(0).getName());
+        Assert.assertEquals("Item 1", "Bregenz", list.get(1).getName());
+        Assert.assertEquals("Item 2", "Salzburg", list.get(2).getName());
+        Assert.assertEquals("Item 3", "Wien", list.get(3).getName());
+
+        list = physicianService.listNearestPhysicians(new GeoLocation("Wien", 48.220778, 16.3100205));
+        Assert.assertEquals("Item 0", "Wien", list.get(0).getName());
+        Assert.assertEquals("Item 1", "Salzburg", list.get(1).getName());
+        Assert.assertEquals("Item 2", "Innsbruck", list.get(2).getName());
+        //Assert.assertEquals("Item 3", "Bregenz", list.get(3).getName());
+
+        list = physicianService.listNearestPhysicians(null);
+    }
+}
